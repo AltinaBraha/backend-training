a2fd7d033152530dde7446a141ffc28f2cbe012d
Altina Braha <altinabraha15@gmail.com>
2025-09-22 14:37:31 +0200

    Add detailed Next.js guide with examples implemented in Visual Studio Code

---
+++ my-app/src/app/(marketing)/layout.js
@@ -0,0 +1,11 @@
+export default function MarketingLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <header>Marketing Header</header>
+        {children}
+        <footer>Marketing Footer</footer>
+      </body>
+    </html>
+  );
+}
